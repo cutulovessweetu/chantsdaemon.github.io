13fd7bef4af80ab76806ae7c7869cd502eed71aa
cutulovessweetu <105921707+cutulovessweetu@users.noreply.github.com>
2026-01-13 22:00:32 +0530

    Delete constants.ts

--- constants.ts
+++
@@ -1,4 +0,0 @@
-export const CHANT_TEXT = 'राधा';
-export const CHANT_RATE_MS = 250; // 4 times per second (1000ms / 4)
-export const RIPPLE_DURATION_MS = 1500; // Time for animation to complete
-export const STORAGE_KEY = 'radha_chant_count';
